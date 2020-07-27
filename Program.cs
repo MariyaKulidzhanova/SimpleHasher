37a88c9a050c94f66eafe408d195a72d09aa3681
Mariya Kulidzhanova <maria.kulidzhanova@gmail.com>
2020-07-27 16:53:10 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Identity;
+namespace PasswordHasherTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var passwordHasher = new PasswordHasher<string>();
+            Console.WriteLine(passwordHasher.HashPassword(null, "heyodamo"));
+            Console.Beep();
+            Console.ReadLine();
+        }
+    }
+}
